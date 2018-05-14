9a7819eb7763a7bfcf403583af5e5b464503ea8b
Zack Middleton <zack@cloemail.com>
2018-05-13 23:21:35 -0500

    Fix compiling flexible hud

--- code/ui/ui_shared.c
+++ code/ui/ui_shared.c
@@ -3640,14 +3640,6 @@ qboolean Item_Bind_HandleKey(itemDef_t *item, int key, qboolean down) {
 
 
 
-void AdjustFrom640(float *x, float *y, float *w, float *h) {
-	//*x = *x * DC->scale + DC->bias;
-	*x *= DC->xscale;
-	*y *= DC->yscale;
-	*w *= DC->xscale;
-	*h *= DC->yscale;
-}
-
 void Item_Model_Paint(itemDef_t *item) {
 	float x, y, w, h;
 	refdef_t refdef;
@@ -3669,7 +3661,7 @@ void Item_Model_Paint(itemDef_t *item) {
 	w = item->window.rect.w-2;
 	h = item->window.rect.h-2;
 
-	AdjustFrom640( &x, &y, &w, &h );
+	DC->adjustFrom640( &x, &y, &w, &h );
 
 	refdef.x = x;
 	refdef.y = y;
@@ -4219,6 +4211,11 @@ void Menu_SetScreenPlacement(menuDef_t *menu, screenPlacement_e hpos, screenPlac
 	if ( !menu )
 		return;
 
+	if (DC->setScreenPlacement == NULL) {
+		Com_Printf( S_COLOR_YELLOW "WARNING: Menu_SetScreenPlacement() is only supported in CGame\n" );
+		return;
+	}
+
 	menu->forceScreenPlacement = qtrue;
 	menu->screenHPos = hpos;
 	menu->screenVPos = vpos;
@@ -4357,7 +4354,7 @@ void Menu_Paint(menuDef_t *menu, qboolean forcePaint) {
 	}
 
 	if (menu->forceScreenPlacement) {
-		CG_SetScreenPlacement( menu->screenHPos, menu->screenVPos );
+		DC->setScreenPlacement( menu->screenHPos, menu->screenVPos );
 	}
 
 	// draw the background if necessary
@@ -4385,7 +4382,7 @@ void Menu_Paint(menuDef_t *menu, qboolean forcePaint) {
 	}
 
 	if (menu->forceScreenPlacement) {
-		CG_PopScreenPlacement();
+		DC->popScreenPlacement();
 	}
 }
 
@@ -5792,6 +5789,11 @@ qboolean MenuParse_screenPlacement( itemDef_t *item, int handle ) {
 	menuDef_t *menu = (menuDef_t*)item;
 	screenPlacement_e hpos, vpos;
 	pc_token_t token;
+
+	if (DC->setScreenPlacement == NULL) {
+		PC_SourceError(handle, "screenPlacement is only supported in HUDs");
+		return qfalse;
+	}
 
 	if (!trap_PC_ReadToken(handle, &token))
 		return qfalse;
