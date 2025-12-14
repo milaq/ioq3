d1d6b83ef9842ce69f714ed0dbbd1d09e51f96df
milaq <mail@milaq.net>
2025-12-14 21:37:43 +0100

    bump version and fix mlq3a buildscript

--- mlq3a-build.sh
+++ mlq3a-build.sh
@@ -5,7 +5,8 @@ commithash=$(git log --pretty=format:'%h' -n 1)
 builddate=$(date +%Y%m%d)
 
 mkdir -p build/mlq3a
-make -j$(nproc) BUILD_SERVER=0 BUILD_CLIENT=0
-pushd build/release-linux-x86_64/baseq3/; zip -r ../../mlq3a/zpak_mlq3a_v${version}.pk3 vm/; popd
+cmake -S . -B build -DCMAKE_BUILD_TYPE=Release
+cmake --build build
+pushd build/Release/baseq3/; zip -r ../../mlq3a/zpak_mlq3a_v${version}.pk3 vm/; popd
 cp mlq3a-readme.md build/mlq3a/zpak_mlq3a_v${version}.txt
 sed -i "2 i\\\nVersion $version (${builddate}.g${commithash})" build/mlq3a/zpak_mlq3a_v${version}.txt
